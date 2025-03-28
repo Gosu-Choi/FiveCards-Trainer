36258cc02db977cee58815ade7461a1ff3da81da
Gosu-Choi <gosu.choi.email@gmail.com>
2025-03-29 01:12:48 +0900

    .possibleRanks

--- FiveCard/client/src/Bot.js
+++ FiveCard/client/src/Bot.js
@@ -120,7 +120,7 @@ const aiDecisionHoldem = async (indicator, survivor, hands, money, pot, is_final
   if (community.length > 0) {
     mention += `\n\n### Community Cards:`;
     if (community.length !== 5){
-      mention += `\n- The board is: ${convertCardList(community)}, so your potential hand range based on current hole cards and possible future community cards is: ${JSON.stringify(calculateHandRange(hands[indicator], community))}, of course the probability is varying.`;
+      mention += `\n- The board is: ${convertCardList(community)}, so your potential hand range based on current hole cards and possible future community cards is: ${JSON.stringify(calculateHandRange(hands[indicator], community).possibleRanks)}, of course the probability is varying.`;
     } else {
       mention += `\n- The board is: ${convertCardList(community)}, so your hand is ${JSON.stringify(calculateHandRange(hands[indicator], community).possibleRanks)}.`;
     }
@@ -284,10 +284,10 @@ const DecisionFBHoldem = async (indicator, survivor, hands, money, pot, is_final
   if (community.length > 0) {
     mention += `\n\n### Community Cards:`;
     if (community.length !== 5){
-      mention += `\n- The board is: ${convertCardList(community)}, so my potential hand range based on current hole cards and possible future community cards is: ${JSON.stringify(calculateHandRange(hands[indicator], community))}, of course the probability is varying.`;
+      mention += `\n- The board is: ${convertCardList(community)}, so my potential hand range based on current hole cards and possible future community cards is: ${JSON.stringify(calculateHandRange(hands[indicator], community).possibleRanks)}, of course the probability is varying.`;
       console.log(mention)
     } else {
-      mention += `\n- The board is: ${convertCardList(community)}, so my hand is ${JSON.stringify(calculateHandRange(hands[indicator], community))}.`;
+      mention += `\n- The board is: ${convertCardList(community)}, so my hand is ${JSON.stringify(calculateHandRange(hands[indicator], community).possibleRanks)}.`;
     }
     mention += `\n- You should assess my hand strength and evaluate possible opponent hands based on this board.`;
   } else {
